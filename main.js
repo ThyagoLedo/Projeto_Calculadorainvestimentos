a1277b695952e466ee9d9534a7aaf71b835a808f
Thyago <thyagopereiraledo@hotmail.com>
2024-09-25 22:45:13 -0300

    Final do dia 25/09

--- main.js
+++ main.js
@@ -5,16 +5,20 @@ const form = document.getElementById("investment-form");
 function renderProgression(evt) {
   evt.preventDefault();
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const additionalContribuition = Number(
-    document.getElementById("additional-contribution").value
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("Return-rate").value);
+  const returnRate = Number(
+    document.getElementById("Return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateRetirnsArray(
     startingAmount,
@@ -27,4 +31,12 @@ function renderProgression(evt) {
 
   console.log(returnsArray);
 }
+function validateInput() {}
+
+for (const formElement of form) {
+  if (formElement.tagName === "IMPUT" && formElement.hasAtrribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
+}
+
 form.addEventListener("submit", renderProgression);
